29a7153910f7cc3a92ad8e103b78125cb990cdc3
Neutx <dlprwz@gmail.com>
2022-12-10 02:14:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white12,
+        appBar: AppBar(
+          backgroundColor: Colors.amberAccent,
+          title: Center(child: Text('Gareebi App')),
+        ),
+        body: Center(child:
+        CachedNetworkImage(
+          //key: UniqueKey(),
+          imageUrl: 'https://assets.weforum.org/article/image/PPYJxokc65xLG-OFdoKeAdByLEZS9GQqDvAmyvW1oS4.jpg',
+
+        ),
+        ),
+      ),
+    ),
+  );
+}
